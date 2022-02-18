516ae2e2b84aeb56bcdb9efa7ff372afce891c22
C1xu <siyuan.xu@hotmail.com>
2022-02-18 10:31:09 +0100

    Modified backend with token authentication

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,38 @@
 import Express from "express";
 import cors from "cors";
+import { v4 as uuid } from "uuid";
 
 const app = Express();
 const PORT = 3001;
 
-app.get("/hello", (req,res) => {
-    const username = req.query.username;
-    const password = req.query.password;
-    console.log(username + " " + password);
-    if( username == "joe" && password == "123"){
-        res.send("Hola Joe");
+// app.get("/hello", (req,res) => {
+//     const username = req.query.username;
+//     const password = req.query.password;
+//     console.log(username + " " + password);
+//     if( username == "joe" && password == "123"){
+//         res.send("Hola Joe");
+//     }else{
+//         res.send("Not Joe");
+//     }
+// });
+
+const secretToken = uuid();
+let requests = 0;
+
+//25ff5d94-183a-4062-ac4c-3e56351e5d3c
+app.get("/secret", (req,res) => {
+    const token = req.query.token;
+    requests++;
+    if( token == secretToken){
+        res.send({code: 200, requests: requests, message:"Hola Secret"});
     }else{
-        res.send("Not Joe");
+        res.send({code: 401, message:"Invalid token"});
     }
 });
 
 app.use(cors());
+
+console.log(uuid());
+
 app.listen(PORT, () =>
     console.log("Server Listening on port: " + PORT));
